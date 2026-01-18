41301c61dfaef0df9b1aaa7765bd5f964e0fe842
aa0986520822-svg <aa0986520822@gmail.com>
2026-01-18 20:45:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -182,4 +182,9 @@ def handle_message(event):
 # ================= RUN =================
 
 if __name__ == "__main__":
-    app.run(port=5000)
+   import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
