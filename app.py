c8772d0a0f1102781aa7bb2c30e606eaaac1fdef
aa0986520822-svg <aa0986520822@gmail.com>
2026-02-04 21:12:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2278,3 +2278,7 @@ h1{{font-size:18px; margin:0 0 12px;}}
 
 # ---- Render 啟動 ----
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    with app.app_context():
+        init_db()
+    app.run(host="0.0.0.0", port=port)
